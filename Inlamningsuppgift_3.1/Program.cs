89562e447f7113dfe50452ced6763833f8001ecb
krbu06001 <krbu06001@E-PW05CGQY>
2024-11-07 15:30:15 +0100

    Add project files.

---
+++ Inlamningsuppgift_3.1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace Inlamning_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur gammal är du?");
+            int år = int.Parse(Console.ReadLine());
+
+            if (år < 16)
+            {
+                Console.WriteLine("Du är tyvärr för ung för att delta i tävlingen");
+            }
+
+            else if (år > 19)
+            {
+                Console.WriteLine("Du är tyvärr för gammal för att delta i tävlingen");
+            }
+
+            else
+            {
+                Console.WriteLine("Du får delta i tävlingen");
+            }
+
+            
+        }
+    }
+}
